69bbc74c7f2a669a129dc28cdf2025e7e131ce64
veun00 <veruniv00@gmail.com>
2025-08-24 15:38:57 +0900

    Fix: API 연동을 위한 카테고리 데이터 수정

--- ywave-vite/src/Data/Industries.tsx
+++ ywave-vite/src/Data/Industries.tsx
@@ -11,10 +11,15 @@ import {
   BiSolidShoppingBagAlt,
   BiDumbbell,
   BiSolidGasPump,
+  BiSolidMicrophone,
+  BiSolidCategory,
+  BiSolidStore,
+  BiCut,
 } from "react-icons/bi";
+import { RiFlowerFill, RiSparklingFill } from "react-icons/ri";
 
 interface Industry {
-  id: string;
+  id: number;
   icon: (props: { size: number }) => React.ReactElement;
   name: string;
   size?: number;
@@ -31,17 +36,29 @@ const ConvenienceIcon = (props: { size: number }) => <BiSolidTime size={props.si
 const ClothingIcon = (props: { size: number }) => <BiSolidShoppingBagAlt size={props.size} />;
 const GymIcon = (props: { size: number }) => <BiDumbbell size={props.size} />;
 const GasIcon = (props: { size: number }) => <BiSolidGasPump size={props.size} />;
+const MicIcon = (props: { size: number }) => <BiSolidMicrophone size={props.size} />;
+const EtcIcon = (props: { size: number }) => <BiSolidCategory size={props.size} />;
+const FlowerIcon = (props: { size: number }) => <RiFlowerFill size={props.size} />;
+const StoreIcon = (props: { size: number }) => <BiSolidStore size={props.size} />;
+const HaircutIcon = (props: { size: number }) => <BiCut size={props.size} />;
+const BeautyIcon = (props: { size: number }) => <RiSparklingFill size={props.size} />;
 
 export const industries: Industry[] = [
-    { id: "restaurant", icon: RestaurantIcon, name: "음식점", size: 16 },
-  { id: "cafe", icon: CafeIcon, name: "카페", size: 16 },
-  { id: "movie", icon: MovieIcon, name: "영화공연", size: 16 },
-  { id: "mart", icon: MartIcon, name: "마트슈퍼", size: 16 },
-  { id: "clothing", icon: ClothingIcon, name: "의류잡화" },
-  { id: "gas", icon: GasIcon, name: "주유소" },
-  { id: "convenience", icon: ConvenienceIcon, name: "생활편의" },
-  { id: "hospital", icon: HospitalIcon, name: "의료" },
-  { id: "stationery", icon: StationeryIcon, name: "교육문구" },
-  { id: "hotel", icon: HotelIcon, name: "숙박" },
-  { id: "gym", icon: GymIcon, name: "체육시설" },
+  { id: 1, icon: MicIcon, name: "오락" },
+  { id: 2, icon: CafeIcon, name: "카페" },
+  { id: 3, icon: RestaurantIcon, name: "음식점" },
+  { id: 4, icon: ConvenienceIcon, name: "생활편의" },
+  { id: 5, icon: MovieIcon, name: "영화/공연" },
+  { id: 6, icon: EtcIcon, name: "기타" },
+  { id: 7, icon: ClothingIcon, name: "의류 잡화" },
+  { id: 8, icon: StationeryIcon, name: "교육/문구" },
+  { id: 9, icon: GasIcon, name: "주유소" },
+  { id: 10, icon: FlowerIcon, name: "꽃집" },
+  { id: 11, icon: StoreIcon, name: "편의점" },
+  { id: 12, icon: MartIcon, name: "슈퍼/마트" },
+  { id: 13, icon: HotelIcon, name: "숙박" },
+  { id: 14, icon: GymIcon, name: "체육시설" },
+  { id: 15, icon: HaircutIcon, name: "헤어샵" },
+  { id: 16, icon: HospitalIcon, name: "의료기관" },
+  { id: 17, icon: BeautyIcon, name: "뷰티" },
 ];
