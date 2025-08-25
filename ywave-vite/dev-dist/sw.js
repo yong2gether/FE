b3244f2a2481fe183f7ea91e9894747e1b4836d4
tlstkdgus <tlstkdgus9@naver.com>
2025-08-25 22:20:42 +0900

    Feat: 버튼 크기 수정

--- ywave-vite/dev-dist/sw.js
+++ ywave-vite/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oem5o6l61g8"
+    "revision": "0.ciuuv82sjp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
